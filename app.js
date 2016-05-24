ce6455fb6fcf2d9708d7ec2f98080d5efea35bc9
Michael Bishop <polishthebishop@gmail.com>
2016-05-23 18:18:24 -0700

    update w/ click event-- comment code

--- app.js
+++ app.js
@@ -10,15 +10,32 @@ function Photo(imgName) {
   this.imgClicked = 0;
   this.imgShown = 0;
 }
-for (var i = 0; i < imgPhotos.length; i++) {  // Counts the length of each object in the array.
-  allProducts.push(new Photo(imgPhotos[i]));  // Pushes info to all products array while creating/generating new photo from imgPhoto array.
+for (var i = 0; i < imgPhotos.length; i++) {  // going to iterate through the array
+  allProducts.push(new Photo(imgPhotos[i]));  // Pushes object instances to all products array while creating object new photo from imgPhoto array.
 }
 //FUNCTION
 function randPic(min,max) { // Creates a random number and returns that value.
   return Math.floor(Math.random() * (max - min) + min);
 }
-var random1 = randPic(0,imgPhotos.length); // randPic starts at 0 in array and picks out a random picture from the length of array.
-console.log(randPic(0, imgPhotos.length));
+var left = document.getElementById('left'); // Assigning variable for javascript that is taken from the left ID tag of HTML.
+var random1 = randPic(0,imgPhotos.length); // randPic starts at 0 in array and picks out a random number from the length of array.
+function displayPic() {
 
-var left = document.getElementById('left'); // Prints the picture to the left id tag of image in html file.
-left.src = allProducts[random1].imgPath; // The left ID of <img> is pulling a source from image path with a random number random1 and using that random number to select a photo.
+  console.log(randPic(0, imgPhotos.length));
+
+  left.src = allProducts[random1].imgPath; // The left ID of <img> is pulling a source from image path with a random number random1 and using that random number to select a photo.
+
+//Will have an image that we can click on and have a click function. Once the option is clicked it will move on to another image.
+
+}
+displayPic();
+
+left.addEventListener('click', handleLeftClick);
+
+function handleLeftClick(event) {
+  allProducts[random1].imgClicked += 1;
+  console.log(allProducts[random1].imgClicked + ' clicks for ' + allProducts[random1].imgName);
+  random1 = randPic(0,imgPhotos.length);
+  // console.log('A picture was clicked');
+  displayPic();
+}
